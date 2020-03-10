5fa1feed71941a9200ed3f683058baa55202beb1
Greg Sarjeant <gsarjeant@gmail.com>
2020-03-09 20:35:34 -0400

    Set s3 bucket policy to force destroy while testing

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ resource "aws_s3_bucket" "tfstate-s3-bucket" {
     bucket        = var.s3_bucket_name
     acl           = "private"
     tags          = var.tags
+    force_destory = true
 
     versioning {
         enabled = true
@@ -30,9 +31,9 @@ resource "aws_s3_bucket" "tfstate-s3-bucket" {
         }
     }
 
-    lifecycle {
-        prevent_destroy = true
-    }
+#    lifecycle {
+#        prevent_destroy = true
+#    }
 }
 
 # Block creation of objects in this bucket with public access
