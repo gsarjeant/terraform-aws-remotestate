d08b27dc30c4d6986960066708cf60bcb1ac882d
Greg Sarjeant <gsarjeant@gmail.com>
2020-03-09 20:38:38 -0400

    typo

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_s3_bucket" "tfstate-s3-bucket" {
     bucket        = var.s3_bucket_name
     acl           = "private"
     tags          = var.tags
-    force_destory = true
+    force_destroy = true
 
     versioning {
         enabled = true
